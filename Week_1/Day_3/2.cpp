edcc6b324de186cd7e90fb649f1c0b674016a267
Prakash Singh <singhprakaash334@gmail.com>
2026-06-03 20:28:42 +0530

    Solve Squares of a Sorted Array Problem
    Implement sortedSquares function to return squares of sorted integers.

---
+++ Week_1/Day_3/2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    // Two pointers:
+    // Compare squares at both ends and place the larger one
+    // at the current position from right to left.
+    // Time: O(n), Space: O(n)
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+
+        int left = 0;
+        int right = n - 1;
+        int pos = n - 1;
+
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare >= rightSquare) {
+                result[pos] = leftSquare;
+                left++;
+            } else {
+                result[pos] = rightSquare;
+                right--;
+            }
+
+            pos--;
+        }
+
+        return result;
+    }
+};

7f33bb8a37c182f94883c6df83abe9fad7e1b5e3
Prakash Singh <singhprakaash334@gmail.com>
2026-06-05 23:08:55 +0530

    Solve Valid Palindrome Problem

---
+++ Week_1/Day_5/1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // Two pointers:
+    // Skip non-alphanumeric characters and compare characters case-insensitively.
+    // Time: O(n), Space: O(1)
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = static_cast<int>(s.size()) - 1;
+
+        while (left < right) {
+            while (left < right && !isalnum(static_cast<unsigned char>(s[left]))) {
+                left++;
+            }
+            while (left < right && !isalnum(static_cast<unsigned char>(s[right]))) {
+                right--;
+            }
+
+            if (tolower(static_cast<unsigned char>(s[left])) !=
+                tolower(static_cast<unsigned char>(s[right]))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};

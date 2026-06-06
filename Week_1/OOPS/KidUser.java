58abd9ce3db8c2ec3598d34efcf0f4e321b9cf53
Prakash Singh <singhprakaash334@gmail.com>
2026-06-06 20:44:46 +0530

    Implement KidUser class for library management

---
+++ Week_1/OOPS/KidUser.java
@@ -0,0 +1,34 @@
+public class KidUser implements LibraryUser {
+
+    private int age;
+    private String bookType;
+
+    public KidUser(int age, String bookType) {
+        this.age = age;
+        this.bookType = bookType;
+    }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
+    public String getBookType() { return bookType; }
+    public void setBookType(String bookType) { this.bookType = bookType; }
+
+    @Override
+    public void registerAccount() {
+        if (age < 12) {
+            System.out.println("You have successfully registered under a Kids Account");
+        } else {
+            System.out.println("Sorry, Age must be less than 12 to register as a kid");
+        }
+    }
+
+    @Override
+    public void requestBook() {
+        if ("Kids".equalsIgnoreCase(bookType)) {
+            System.out.println("Book Issued successfully, please return the book within 10 days");
+        } else {
+            System.out.println("Oops, you are allowed to take only kids books");
+        }
+    }
+}

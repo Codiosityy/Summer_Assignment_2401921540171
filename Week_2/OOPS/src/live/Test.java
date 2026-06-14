5f5384aac21660c20026979c0951d10d56bc63ec
Prakash Singh <singhprakaash334@gmail.com>
2026-06-14 16:00:55 +0530

    Implement Test class to demonstrate Playable interface

---
+++ Week_2/OOPS/src/live/Test.java
@@ -0,0 +1,26 @@
+package live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+    public static void main(String[] args) {
+        // a. Create an instance of Veena and call play()
+        Veena veena = new Veena();
+        veena.play();
+
+        // b. Create an instance of Saxophone and call play()
+        Saxophone saxophone = new Saxophone();
+        saxophone.play();
+
+        // c. Place the above instances in a variable of type Playable and then call play()
+        Playable playable;
+
+        playable = veena;
+        playable.play();
+
+        playable = saxophone;
+        playable.play();
+    }
+}

9f1dab48652457bc859f1914b6218b5a37bf8995
Prakash Singh <singhprakaash334@gmail.com>
2026-06-22 20:31:47 +0530

    Add FirstClass compartment with notice method

---
+++ Week_3/OOPS/src/compartment/FirstClass.java
@@ -0,0 +1,8 @@
+package compartment;
+
+public class FirstClass extends Compartment {
+    @Override
+    public String notice() {
+        return "First Class compartment: Comfortable seating and premium service.";
+    }
+}

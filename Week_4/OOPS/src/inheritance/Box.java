558e6e91ab2cb1341b2c9e31e4dd9e34ae363d30
Prakash Singh <singhprakaash334@gmail.com>
2026-06-29 06:58:36 +0530

    Add Box class for calculating area

---
+++ Week_4/OOPS/src/inheritance/Box.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Box {
+
+    protected double length;
+    protected double breadth;
+
+    public Box(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public double area() {
+        return length * breadth;
+    }
+}
